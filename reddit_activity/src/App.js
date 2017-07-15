d274d83e8ee25d4ea8771e5ed7885bde1171d293
beebeean09 <beebeean09@gmail.com>
2017-07-14 22:25:06 -0700

    remove uncomment code

--- reddit_activity/src/App.js
+++ reddit_activity/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
 import './App.css';
 import Main from './components/main.jsx';
 
@@ -13,12 +12,5 @@ class App extends Component {
     );
   }
 }
-// <div className="App-header">
-//   <img src={logo} className="App-logo" alt="logo" />
-//   <h2>Welcome to React</h2>
-// </div>
-// <p className="App-intro">
-//   To get started, edit <code>src/App.js</code> and save to reload.
-// </p>
 
 export default App;
