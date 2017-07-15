180732f411fe2b89371120738454a46ec9f51d4f
beebeean09 <beebeean09@gmail.com>
2017-07-14 22:24:46 -0700

    change color of title and remove app style

--- reddit_activity/src/App.js
+++ reddit_activity/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
-// import './App.css';
+import './App.css';
 import Main from './components/main.jsx';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <h1>REDDIT ACTIVITY</h1>
         <Main />
       </div>
     );
