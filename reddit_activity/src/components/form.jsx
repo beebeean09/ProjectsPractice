14ff3520075a55211e0dda5e83fa45aac75a1833
beebeean09 <beebeean09@gmail.com>
2017-07-14 18:29:20 -0700

    main and form components

---
+++ reddit_activity/src/components/form.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class RedditForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    return(
+      <div>
+        <h1>Reddit Form!</h1>
+      
+      </div>
+    );
+  }
+}
+
+export default RedditForm;
---
+++ reddit_activity/src/components/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import RedditForm from './form.jsx';
+
+class Main extends React.Component {
+
+  render() {
+    return (
+      <div className="main-container">
+        <RedditForm />
+      </div>
+    );
+  }
+}
+
+export default Main;
