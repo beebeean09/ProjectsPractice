1d52a8bfdef1f2fccee237b96c7dacfa4fd3fdbe
beebeean09 <beebeean09@gmail.com>
2017-07-20 22:13:37 -0700

    change file to index html;

--- JS30Day/arrayProblems/arrays.js
+++
@@ -1,7 +0,0 @@
-const DOB = [{first: 'Kristen', last: 'Lee', yob: 1994}, {first: 'Vivian', last: 'Lee', yob: 1991},
-            {first: 'Sharon', last: 'Lee', yob: 1999}, {first: 'Susan', last: 'Leightner', yob: 1977},
-            {first: 'Samantha', last: 'Lee', yob: 2001}];
-const names = ['Vivian', 'Andrew', 'Sharon', 'Kristen', 'Andy', 'James', 'Vicky', 'David', 'Sherry', 'Barry'];
-
-// Array.prototype.filter()
-const nineteen = DOB.filter();
