ed4591876668587e4137395c02886d4e31dd5456
beebeean09 <beebeean09@gmail.com>
2017-08-08 21:57:14 -0700

    skip add from data set to curren time of video

--- JS30Day/customVidPlayer/scripts.js
+++ JS30Day/customVidPlayer/scripts.js
@@ -21,9 +21,16 @@ function updateButton() {
   const icon = this.paused ? 'Play' : 'Pause';
   toggle.textContent = icon;
 }
+
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip);
+
+}
 // Hook up event listeners
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 
 toggle.addEventListener('click', togglePlay);
+
+skipButtons.forEach(button => button.addEventListener('click', skip));
