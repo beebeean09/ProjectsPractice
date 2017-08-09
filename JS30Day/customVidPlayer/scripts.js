6e7a961e9f34c4f697c3bfe0c79a57d628609548
beebeean09 <beebeean09@gmail.com>
2017-08-08 22:29:03 -0700

    add styling for progress bar to update

--- JS30Day/customVidPlayer/scripts.js
+++ JS30Day/customVidPlayer/scripts.js
@@ -27,18 +27,23 @@ function skip() {
 }
 
 function handleRangeUpdate() {
-  console.log('range');
-  console.log(this.name);
-  console.log(this.value);
   video[this.name] = this.value;
+}
+
+function handleProgressBar() {
+  const percent = (video.currentTime / video.duration) * 100;
+  console.log(percent);
+  progressBar.style.flexBasis = `${percent}%`;
 }
 // Hook up event listeners
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgressBar);
 
 toggle.addEventListener('click', togglePlay);
 
 skipButtons.forEach(button => button.addEventListener('click', skip));
+
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
 ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
