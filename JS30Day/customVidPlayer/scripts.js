972890772b272d38ea99ef769485a4fb2e20c237
beebeean09 <beebeean09@gmail.com>
2017-08-08 21:47:23 -0700

    updateButton for play pause

--- JS30Day/customVidPlayer/scripts.js
+++ JS30Day/customVidPlayer/scripts.js
@@ -10,12 +10,20 @@ const ranges = player.querySelectorAll('.player-slider');
 // Build out functions
 
 function togglePlay() {
-  console.log(event);
   if (video.paused) {
     video.play();
   } else {
     video.pause();
   }
 }
+
+function updateButton() {
+  const icon = this.paused ? 'Play' : 'Pause';
+  toggle.textContent = icon;
+}
 // Hook up event listeners
 video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+
+toggle.addEventListener('click', togglePlay);
