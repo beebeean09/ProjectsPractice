d671da565264d0b148afc9e50acfd3a04a1c912b
beebeean09 <beebeean09@gmail.com>
2017-08-09 23:13:51 -0700

    add delete

--- JS30Day/problems/ll.rb
+++ JS30Day/problems/ll.rb
@@ -13,6 +13,31 @@ class LinkedList
     @head = Node.new(val, nil)
   end
 
+  def add(val)
+    current = @head
+    while current.next != nil
+      current = current.next
+    end
+    current.next = Node.new(val, nil)
+  end
 
+  def delete(val)
+    p 'delete'
+    current = @head
+    p current
+    current.next = @head
+    p 'next'
+    p current.next
+    if current.val = val
+      @head = current.next
+    else
+      while (current.next != nil) && (current.next.val != val)
+        current = current.next
+      end
+      unless current.next == nil
+        current.next = current.next.next
+      end
+    end
+  end
 
 end
