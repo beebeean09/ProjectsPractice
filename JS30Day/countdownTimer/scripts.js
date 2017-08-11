0640070336e72f6582a4ae0b8b155c769d671484
beebeean09 <beebeean09@gmail.com>
2017-08-11 00:23:10 -0700

    adjust 0 padding for end time

--- JS30Day/countdownTimer/scripts.js
+++ JS30Day/countdownTimer/scripts.js
@@ -1,12 +1,13 @@
 let countdown;
 const timerDisplay = document.querySelector('.display-time-left');
+const endTime = document.querySelector('.display-end-time');
 
 function timer(seconds) {
   const now = Date.now();
   const then = now + seconds * 1000;
   // Need this here since the interval wont countdown until after 1 sec
   displayTimeLeft(seconds);
-
+  displayEndTime(then);
   // Giving setInterval a variable so we can clear it at 0 secs
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -23,6 +24,15 @@ function displayTimeLeft(seconds) {
   const minutes = Math.floor(seconds / 60);
   const remainderSecs = (seconds % 60);
   const display = `${minutes < 10 ? '0': ''}${minutes}:${remainderSecs < 10 ? '0' : ''}${remainderSecs}`;
+  document.title = display;
   timerDisplay.textContent = display;
-  console.log({minutes, remainderSecs});
+}
+
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp);
+  const hour = end.getHours();
+  const minutes = end.getMinutes();
+  const adjustedHour = hour > 12 ? 24 - hour : hour;
+  endTime.textContent = `Be back at ${adjustedHour}:${minutes < 10 ? '0': ''}${minutes}.`;
+
 }
