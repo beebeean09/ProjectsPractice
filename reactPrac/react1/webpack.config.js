a4f99546858926b9b65381475a44f399cf04cfac
beebeean09 <beebeean09@gmail.com>
2017-08-10 20:33:10 -0700

    add dependencies and webpack config

---
+++ reactPrac/react1/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+
+module.exports = {
+  context: __dirname,
+  entry: './components/app.js',
+  output: {
+    path: path.resolve(__dirname, 'assets'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  }
+};
