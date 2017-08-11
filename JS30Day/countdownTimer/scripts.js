97f82715a1d4fac244b14d31523473631f8ca686
beebeean09 <beebeean09@gmail.com>
2017-08-10 23:38:46 -0700

    counts down from giving seconds

--- JS30Day/countdownTimer/scripts.js
+++ JS30Day/countdownTimer/scripts.js
@@ -1,6 +1,17 @@
+let countdown;
+
 function timer(seconds) {
   const now = Date.now();
-  console.log(now);
   const then = now + seconds * 1000;
-  console.log(then);
+
+  // Giving setInterval a variable so we can clear it at 0 secs
+  countdown = setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+      // check if we need to stop by seeing if < 0
+      if (secondsLeft < 0) {
+        clearInterval(countdown);
+        return;
+      }
+    console.log(secondsLeft);
+  }, 1000);
 }
